cb7d607c63d33baa2a0666ae013d816d1b9dd8e2
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-08-21 15:29:01 +0800

    feat: add async graphql

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
-fn main() {
-    println!("Hello, world!");
+pub mod token;
+
+use async_graphql::{
+    http::{playground_source, GraphQLPlaygroundConfig, ALL_WEBSOCKET_PROTOCOLS},
+    EmptyMutation, Schema,
+};
+use async_graphql_axum::{GraphQLProtocol, GraphQLRequest, GraphQLResponse, GraphQLWebSocket};
+use axum::{
+    extract::{ws::WebSocketUpgrade, State},
+    http::header::HeaderMap,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Router, Server,
+};
+
+use token::{on_connection_init, QueryRoot, SubscriptionRoot, Token, TokenSchema};
+
+async fn graphql_playground() -> impl IntoResponse {
+    Html(playground_source(
+        GraphQLPlaygroundConfig::new("/").subscription_endpoint("/ws"),
+    ))
+}
+
+fn get_token_from_headers(headers: &HeaderMap) -> Option<Token> {
+    headers
+        .get("Token")
+        .and_then(|value| value.to_str().map(|s| Token(s.to_string())).ok())
+}
+
+async fn graphql_handler(
+    State(schema): State<TokenSchema>,
+    headers: HeaderMap,
+    req: GraphQLRequest,
+) -> GraphQLResponse {
+    let mut req = req.into_inner();
+    if let Some(token) = get_token_from_headers(&headers) {
+        req = req.data(token);
+    }
+    schema.execute(req).await.into()
+}
+
+async fn graphql_ws_handler(
+    State(schema): State<TokenSchema>,
+    protocol: GraphQLProtocol,
+    websocket: WebSocketUpgrade,
+) -> Response {
+    websocket
+        .protocols(ALL_WEBSOCKET_PROTOCOLS)
+        .on_upgrade(move |stream| {
+            GraphQLWebSocket::new(stream, schema.clone(), protocol)
+                .on_connection_init(on_connection_init)
+                .serve()
+        })
+}
+
+#[tokio::main]
+async fn main() {
+    let schema = Schema::new(QueryRoot, EmptyMutation, SubscriptionRoot);
+
+    let app = Router::new()
+        .route("/", get(graphql_playground).post(graphql_handler))
+        .route("/ws", get(graphql_ws_handler))
+        .with_state(schema);
+
+    println!("Playground: http://localhost:8000");
+
+    Server::bind(&"127.0.0.1:8000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
