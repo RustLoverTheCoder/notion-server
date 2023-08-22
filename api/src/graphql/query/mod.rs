90774a21afe48a0fd62ac71baea1abe399f60c82
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-08-22 11:33:34 +0800

    feat: add query root

--- api/src/graphql/query/mod.rs
+++ api/src/graphql/query/mod.rs
@@ -1,3 +1,16 @@
+pub mod block;
 pub mod user;
 pub mod user_setting;
-pub mod block;
+
+use async_graphql::{Context, Object};
+
+pub struct QueryRoot;
+
+pub struct Token(pub String);
+
+#[Object]
+impl QueryRoot {
+    async fn current_token<'a>(&self, ctx: &'a Context<'_>) -> Option<&'a str> {
+        ctx.data_opt::<Token>().map(|token| token.0.as_str())
+    }
+}
--- api/src/graphql/schema.rs
+++ api/src/graphql/schema.rs
@@ -1,17 +1,8 @@
-use async_graphql::{Context, EmptyMutation, EmptySubscription, Object, Schema};
+use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+
+use crate::graphql::query::QueryRoot;
 
 pub type AppSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
-
-pub struct QueryRoot;
-
-pub struct Token(pub String);
-
-#[Object]
-impl QueryRoot {
-    async fn current_token<'a>(&self, ctx: &'a Context<'_>) -> Option<&'a str> {
-        ctx.data_opt::<Token>().map(|token| token.0.as_str())
-    }
-}
 
 pub fn build_schema() -> AppSchema {
     let schema = Schema::new(QueryRoot, EmptyMutation, EmptySubscription);
