d5a8623396c2ccb68bd7c100265d7287dac14164
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-08-22 11:13:28 +0800

    feat: remove no use

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,7 +1,5 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_table;
-mod m20230821_093148_create_notes;
 mod m20230821_102932_create_user;
 mod m20230822_023238_create_block;
 
@@ -11,8 +9,6 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20220101_000001_create_table::Migration),
-            Box::new(m20230821_093148_create_notes::Migration),
             Box::new(m20230821_102932_create_user::Migration),
             Box::new(m20230822_023238_create_block::Migration),
         ]
