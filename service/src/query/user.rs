1696b477c405b99a16fd115e28f4cc2db16cb654
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-08-23 14:03:47 +0800

    feat: add user query service

--- service/src/query/user.rs
+++ service/src/query/user.rs
@@ -1,6 +1,10 @@
 use ::entity::{user, user::Entity as User};
-use sea_orm::*;
+use sea_orm::entity::prelude::*;
 
 pub struct UserQuery;
 
-impl UserQuery {}
+impl UserQuery {
+    pub async fn find_user_by_id(conn: &DbConn, id: Uuid) -> Result<Option<user::Model>, DbErr> {
+        User::find_by_id(id).one(conn).await
+    }
+}
