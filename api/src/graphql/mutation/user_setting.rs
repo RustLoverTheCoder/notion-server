2b165291267d15df9f03dd0e8d0ac111bc1022bc
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-08-24 17:28:49 +0800

    feat: add update_user_setting

--- api/src/graphql/mutation/user_setting.rs
+++ api/src/graphql/mutation/user_setting.rs
@@ -1,5 +1,9 @@
-use async_graphql::{Context, InputObject, Object, Result, SimpleObject, ID};
-use entity::user_setting;
+use async_graphql::{Context, InputObject, Object, Result};
+use config::contants::DB;
+use entity::user::Entity as User;
+use entity::{user_setting::Entity as UserSetting,user_setting};
+use sea_orm::{Set, ActiveModelTrait};
+use sea_orm::prelude::Uuid;
 
 #[derive(Default)]
 pub struct UserSettingMutation;
@@ -12,7 +16,39 @@ pub struct NewUserSettingInput {
 
 #[Object]
 impl UserSettingMutation {
-    async fn update_user_setting(&self, ctx: &Context<'_>, new_user_setting: NewUserSettingInput) -> Result<bool> {
-        todo!()
+    async fn update_user_setting(
+        &self,
+        ctx: &Context<'_>,
+        new_user_setting: NewUserSettingInput,
+    ) -> Result<bool> {
+        let db = DB.get().unwrap();
+        let user_id = ctx.data::<Uuid>().unwrap();
+        let user_data = User::find_by_id(user_id.to_owned()).one(db).await?;
+        match user_data {
+            Some(user) => {
+                let settings_id = user.settings_id.to_owned();
+                match settings_id {
+                    Some(id) => {
+                        let setting = UserSetting::find_by_id(id).one(db).await?;
+                        match setting {
+                            Some(setting) => {
+                                let mut setting_active: user_setting::ActiveModel = setting.into();
+                                if let Some(color_scheme) = new_user_setting.color_scheme {
+                                    setting_active.color_scheme = Set(color_scheme.to_owned());
+                                }
+                                if let Some(language) = new_user_setting.language {
+                                    setting_active.language = Set(language.to_owned());;
+                                }
+                                setting_active.update(db).await?;
+                                Ok(true)
+                            },
+                            None => Ok(false),
+                        }
+                    }
+                    None => Ok(false),
+                }
+            }
+            None => Ok(false),
+        }
     }
 }
